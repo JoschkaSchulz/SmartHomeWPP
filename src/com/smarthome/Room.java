2c5c1dc5b4e1b114b36e970a365e3a93d4d6d548
Joschka Schulz <Horstyschulz@gmail.com>
2012-11-09 13:36:07 +0100

    Added a constructor for Room

--- src/com/smarthome/Room.java
+++ src/com/smarthome/Room.java
@@ -5,6 +5,12 @@ import java.util.LinkedList;
 public class Room {
 	public float x, y;
 	public LinkedList<Gesture> gestures = new LinkedList<Gesture>();
+	
+	public Room(float x, float y) {
+		this.x = x;
+		this.y = y;
+	}
+	
 	public void fire(float x, float y, SmartHomeActivity activity) {
 		for (Gesture gesture: gestures) {
 			gesture.fire(x, y, activity);
