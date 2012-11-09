669abdf33c95225dca6f7c0e3a489da02a3ef604
Bjoern Eberhardt <lazer.erazer@gmail.com>
2012-11-09 13:37:20 +0100

    RoomGesture Constructor

--- src/com/smarthome/Gesture.java
+++ src/com/smarthome/Gesture.java
@@ -2,6 +2,13 @@ package com.smarthome;
 
 public abstract class Gesture {
 	public float x1, y1, x2, y2;
+	public Gesture() {}
+	public Gesture(float x1, float y1, float x2, float y2) {
+		this.x1 = x1;
+		this.y1 = y1;
+		this.x2 = x2;
+		this.y2 = y2;
+	}
 	public boolean match(float x, float y) {
 		return x > x1 && x < x2 && y > y1 && y < y2;
 	}
