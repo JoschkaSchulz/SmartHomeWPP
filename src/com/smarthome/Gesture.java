f665b6436be7c45b5d866f8f00a669e73eb04ced
Bjoern Eberhardt <lazer.erazer@gmail.com>
2012-11-09 14:04:38 +0100

    screen size calculation

--- src/com/smarthome/Gesture.java
+++ src/com/smarthome/Gesture.java
@@ -4,10 +4,10 @@ public abstract class Gesture {
 	public float x1, y1, x2, y2;
 	public Gesture() {}
 	public Gesture(float x1, float y1, float x2, float y2) {
-		this.x1 = x1;
-		this.y1 = y1;
-		this.x2 = x2;
-		this.y2 = y2;
+		this.x1 = x1 * 800 / 800;
+		this.y1 = y1 * 800 / 800;
+		this.x2 = x2 * 480 / 480;
+		this.y2 = y2 * 480 / 480;
 	}
 	public boolean match(float x, float y) {
 		return x > x1 && x < x2 && y > y1 && y < y2;
--- src/com/smarthome/LightGesture.java
+++ src/com/smarthome/LightGesture.java
@@ -1,6 +1,10 @@
 package com.smarthome;
 
 public class LightGesture extends Gesture {
+	public LightGesture() {}
+	public LightGesture(float x1, float y1, float x2, float y2) {
+		super(x1, y1, x2, y2);
+	}
 	public void click(SmartHomeActivity activity) {
 		
 	}
